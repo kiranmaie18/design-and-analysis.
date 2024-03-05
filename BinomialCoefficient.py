5658307f4901f216b6a7be98bf36e26ea92f38e3
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2024-03-05 20:29:56 +0530

    Add files via upload

---
+++ BinomialCoefficient.py
@@ -0,0 +1,16 @@
+def binomial_coefficient(n, k):
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    for i in range(n + 1):
+        dp[i][0] = 1
+    for j in range(k + 1):
+        dp[j][j] = 1
+    for i in range(1, n + 1):
+        for j in range(1, min(i, k) + 1):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+
+    return dp[n][k]
+n = 5
+k = 2
+
+result = binomial_coefficient(n, k)
+print(f"C({n}, {k}) =", result)
