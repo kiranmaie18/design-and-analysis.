f506d490e64b14293a29e485e37276f678366b7c
kiranmaie18 <123735782+kiranmaie18@users.noreply.github.com>
2024-03-05 20:28:43 +0530

    Add files via upload

---
+++ Floyd.py
@@ -0,0 +1,19 @@
+def floyd_algorithm(graph):
+    n = len(graph)
+    dist = [row[:] for row in graph]
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+    return dist
+graph = [
+    [0, 5, float('inf'), 10],
+    [float('inf'), 0, 3, float('inf')],
+    [float('inf'), float('inf'), 0, 1],
+    [float('inf'), float('inf'), float('inf'), 0]
+]
+
+result = floyd_algorithm(graph)
+for row in result:
+    print(row)
